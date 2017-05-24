1c1ea60d328e2c5375d6ca55e5b80fe2335886f1
Seunghun Lee <waydi1@gmail.com>
2017-05-25 00:41:06 +0900

    Make basic database structure and Post model

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 const app = express();
+const db = mongoose.createConnection('mongodb://localhost:27017/simpleblog');
 
-app.get('/', (req, res) => {
-  res.end('Hello World');
+const postSchema = new mongoose.Schema({
+  subject: String,
+  content: String,
+  created: { type: Date, default: Date.now }
+});
+
+const Post = db.model('Post', postSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/posts', (req, res) => {
+  Post.find({}).exec((err, posts) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(posts);
+    }
+  });
+});
+
+app.post('/posts', (req, res) => {
+  const post = new Post({
+    subject: req.body.subject,
+    content: req.body.content
+  });
+  post.save();
+
+  res.redirect('/posts');
 });
 
 module.exports = app;
